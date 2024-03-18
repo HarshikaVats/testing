0429497aec4ab76cc57111996380b267f209dfd5
Harshika Vats <harshivt@amazon.com>
2024-03-18 18:30:16 +0530

    My first commit

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* Steps taken to run the code:
+    1. g++ -o fileName.exe hello.cpp
+    2. ./fileName.exe
+
+    OR
+
+    Click on RUN CODE button
+    */
+    cout << "Sam is the best person in the universe";
+    cout << "Added this into demo branch";
+    return 0;
+}
